d2cda6fbb0224882c43fcc552c2731ed0941c87f
KIYONGA <Kiyongastanley@gmail.com>
2023-10-05 20:07:39 -0700

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - the main function
+ * main - main function
  *
- * Return: 0 when successful
+ * Return: success 0
  */
-
-int main (void)
+int main(void)
 {
-      puts ("\"Programming is like building a multilingual puzzle");
-      return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	Return (0);
 }
