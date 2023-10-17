9e96ef510e65e4006f14947c0986c725b10a1dab
KIYONGA <Kiyongastanley@gmail.com>
2023-10-17 15:48:06 -0700

    Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 12
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char password[PASSWORD_LENGTH + 1];
+
+	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+	int i;
+
+	srand(time(NULL));
+
+	for (i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		int index = rand() % (int)(sizeof(charset) - 1);
+		password[i] = charset[index];
+	}
+
+	password[PASSWORD_LENGTH] = '\0';
+
+	printf("Generated Password: %s\n", password);
+
+	return (0);
+}
+
