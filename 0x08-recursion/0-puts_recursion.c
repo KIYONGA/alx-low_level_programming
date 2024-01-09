b30af3a7e322f0907dd90ebe16126141cdbb6fa8
KIYONGA <Kiyongastanley@gmail.com>
2024-01-08 21:34:41 -0800

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,17 @@
-#include <unistd.h>
-
 /**
- * _puts_recursion - Prints a string, followed by a new line.
- * @s: The string to be printed.
+ * _puts_recursion: -prints a string followed by a new line.
+ * @s:The string to be printed.
+ *
+ * Return: returns a new line
  */
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
-
-	if (*s == '\0');
+	if (*s == '\0')
 	{
 		write(1, "\n", 1);
-		return;
+			return;
 	}
 	write(1, s, 1);
+
 	_puts_recursion(s + 1);
 }
-
